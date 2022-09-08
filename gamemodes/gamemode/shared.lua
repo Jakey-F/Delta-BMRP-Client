c7fb00845d1f0e7ab79b409c321e15f273e0c73d
JakeyF_ <jakeyfloofle@gmail.com>
2022-09-08 14:25:40 +0200

    Client files for BMRP Gamemode

---
+++ gamemodes/gamemode/shared.lua
@@ -0,0 +1,13 @@
+AddCSLuaFile()
+
+GM.Name			= "Delta | Black Mesa Roleplay"
+GM.Author		= "Jakey#0673"
+
+GM.Version      = "Public"
+GM.Development  = false
+
+DeriveGamemode("darkrp")
+
+-- Server Workshop Collection & Discord Invite Code
+GM.idCollection = "2859850893"
+GM.idDiscord = "4tbDpMybBU"
